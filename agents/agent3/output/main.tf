812ce194be746cdf1bd190e418f08f29e5a50f81
umar <muhammadumarfayaaz@gmail.com>
2026-07-28 22:05:53 +0530

    Updated agent9 and generator changes

--- agents/agent3/output/main.tf
+++ agents/agent3/output/main.tf
@@ -2,11 +2,24 @@ module "log_analytics_workspace_1" {
 
   source = "../../../modules/log_analytics_workspace"
 
-  name                = "law-eva-uat"
-  location            = "East US"
+  name = "law-eva-qa"
+  location = "East US"
   resource_group_name = "rg-demo"
   tags = {
-    Project = "Exelixis"
-  }
+  Project = "Exelixis"
+}
+
+}
+
+module "log_analytics_workspace_2" {
+
+  source = "../../../modules/log_analytics_workspace"
+
+  name = "law-eva-uat"
+  location = "East US"
+  resource_group_name = "rg-demo"
+  tags = {
+  Project = "Exelixis"
+}
 
 }
