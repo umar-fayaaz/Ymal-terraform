61917a0494baf78803c066ab1ba34548b38bf265
umar <muhammadumarfayaaz@gmail.com>
2026-07-28 19:23:31 +0530

    test2 agent5

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -4,4 +4,5 @@ resource "azurerm_key_vault" "this" {
   resource_group_name = var.resource_group_name
   tenant_id           = var.tenant_id
   sku_name            = var.sku_name
+  tags                  = var.tags
 }
--- modules/key_vault/variables.tf
+++ modules/key_vault/variables.tf
@@ -3,3 +3,6 @@ variable "location" {}
 variable "resource_group_name" {}
 variable "tenant_id" {}
 variable "sku_name" {}
+variable "tags" {
+  type = map(string)
+}
